5fc6f2d53eec41a76afd5f1820d49865a99f6fc3
pontus <pontus.carlsson@gmail.com>
2017-05-01 23:04:08 +0200

    compare with lowercase

--- src/main/java/org/freedesktop/dbus/bin/IdentifierMangler.java
+++ src/main/java/org/freedesktop/dbus/bin/IdentifierMangler.java
@@ -11,7 +11,7 @@ public final class IdentifierMangler {
     private static String[] keywords = new String[]{"class", "true", "false", "null", "abstract", "continue", "for", "new", "switch", "assert", "default", "goto", "package", "synchronized", "boolean", "do", "if", "private", "this", "break", "double", "implements", "protected", "throw", "byte", "else", "import", "public", "throws", "case", "enum", "instanceof", "return", "transient", "catch", "extends", "int", "short", "try", "char", "final", "interface", "static", "void", "class", "finally", "long", "strictfp", "volatile", "const", "float", "native", "super", "while"};
 
     public static String mangle(String name) {
-        if(Arrays.binarySearch(keywords, name) >= 0) {
+        if(Arrays.binarySearch(keywords, name.toLowerCase()) >= 0) {
             name = "_" + name;
         }
 
